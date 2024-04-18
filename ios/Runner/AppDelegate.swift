307dcff522c75de72932e1c96b957c06dffff312
Phan Anh Vu <phananhvu12a5chilang@gmail.com>
2024-04-19 00:26:23 +0700

    edit detail_page_offline

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+// import FirebaseCore
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,6 +11,7 @@ import GoogleMaps
     
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyBeXmfS_J09cX3frhZTzOHu1iw5YWDOM08")
+    // FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     //Change
     if #available(iOS 10.0, *) {
